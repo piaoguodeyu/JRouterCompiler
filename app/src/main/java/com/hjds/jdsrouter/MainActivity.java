4ea91be1a2058508074c48d99859d48aad3dc1e7
zhangxiaowei <>
2020-03-22 18:28:10 +0800

    修复多模块跳转失败问题

--- app/src/main/java/com/hjds/jdsrouter/MainActivity.java
+++ app/src/main/java/com/hjds/jdsrouter/MainActivity.java
@@ -20,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.tttttt);
         textView.setText(this.getClass().getName());
-        Object fragment = JRouter.build("TestPresent").navigation();
-        Log.e("fragmentfragment", " 11111111= " + fragment.getClass().getName());
-        textView.setText(fragment.getClass().getName());
+//        Object fragment = JRouter.build("TestPresent").navigation();
+//        Log.e("fragmentfragment", " 11111111= " + fragment.getClass().getName());
+//        textView.setText(fragment.getClass().getName());
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
